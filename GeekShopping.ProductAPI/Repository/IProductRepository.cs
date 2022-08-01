cdfd99b802a9f7748a81c66e6673b49bab8f891b
Caio Peres <caioperescs@hotmail.com>
2022-08-01 16:42:05 -0300

    First commit

---
+++ GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.ProductAPI.Data.DTOs;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDTO>> FindAll();
+        Task<ProductDTO> FindById(long id);
+        Task<ProductDTO> Create(ProductDTO productDTO);
+        Task<ProductDTO> Update(ProductDTO productDTO);
+        Task<bool> Delete(long id);
+
+    }
+}
